7f070fe026e869c06e5b45357b13f2ad4c5f1bef
Kazuki Nakamichi <hw20a086@oecu.jp>
2022-12-16 11:01:05 +0900

    Initial Commit

---
+++ C++PE2022-Final/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++PE2022-Final
+//
+//  Created by hw20a086 on 2022/12/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
